711c2b9c17d9caa46ba594ff566fd54bd13a3123
eldaroid <eldario10@mail.ru>
2021-01-07 16:08:41 +0300

    start

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Эльдар Попов on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("hhh")
+    }
+    
+    func flipCard(withEmoji Emoji: String, on button: UIButton) {
+        
+    }
+    
+}
+
